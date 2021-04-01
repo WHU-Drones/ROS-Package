0a38a69f523b2cba0842cee5a489867af20322e0
bird1and1fish <1014693892@qq.com>
2021-04-01 20:13:36 +0800

    update link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # sleep 10
 sudo chmod 777 /dev/ttyTHS0
-sudo chmod 777 /dev/ttyTHS2
 
 source /opt/ros/melodic/setup.bash
 source /home/nvidia/Code/catkin_ws/devel/setup.bash
 source /home/nvidia/Code/cartographer_ws/install_isolated/setup.bash
+source /home/nvidia/Code/mavros_ws/devel/setup.bash
 
 roslaunch whud_union whud_union.launch
