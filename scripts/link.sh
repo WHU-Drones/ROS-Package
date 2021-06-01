fea79e1e07b3c2ab93e68d4fbf614366968b7133
bird1and1fish <1014693892@qq.com>
2021-06-01 11:37:22 +0800

    fix bug

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# sleep 10
+sleep 5
 sudo chmod 777 /dev/ttyTHS0
+python cargo_gpio_init.py 
 
 source /opt/ros/melodic/setup.bash
 source /home/nvidia/Code/catkin_ws/devel/setup.bash
 source /home/nvidia/Code/cartographer_ws/install_isolated/setup.bash
 source /home/nvidia/Code/mavros_ws/devel/setup.bash
 
-roslaunch whud_union whud_union.launch
+
+# roslaunch whud_union whud_union.launch
